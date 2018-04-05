4695de480b6c6a769c520e3a580845de557f816b
William Jefferies <willjefferies10@gmail.com>
2018-04-05 14:50:19 -0600

    2. Images placed

---
+++ content/game.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+
+
+var canvas = document.getElementById("layer1");
+var context = canvas.getContext("2d");
+
+
+var sprite1 = new Image();
+var sprite2 = new Image();
+
+sprite1.onload = function() {
+	context.drawImage(sprite1,50,650);
+	context.drawImage(sprite2,800,650);
+	};
+
+sprite1.src = 'kitty1.png';
+sprite2.src = 'kitty2.png';
+
+};
